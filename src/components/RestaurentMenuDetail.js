db56f34d563b88fb42ff3592114ba2fa14e79954
theripper1116 <52823649+theripper1116@users.noreply.github.com>
2024-04-23 17:04:55 +0530

    April 23 commit 4

--- src/components/RestaurentMenuDetail.js
+++ src/components/RestaurentMenuDetail.js
@@ -1,11 +1,11 @@
 import { DISHES_IMAGE_URL } from "../utils/Links";
 
-const RestaurentMenuDetail = ({restaurentMenuData}) => {
+const RestaurentMenuDetail = ({ restaurentMenuData }) => {
   const {
     price = "Not Available",
     name = "Not Available",
     imageId = "Not Available",
-    description = "Not Avaiable"
+    description = "Not Avaiable",
   } = restaurentMenuData?.card?.info;
   return (
     <div className="card m-1" style={{ width: 200, height: 450 }}>
@@ -16,7 +16,7 @@ const RestaurentMenuDetail = ({restaurentMenuData}) => {
       />
       <div className="card-body">
         <h4>{name}</h4>
-        <h5>{price}</h5>
+        {price != "Not Available" ? <h5>{price / 100}</h5> : <h5>{price}</h5>}
         <h5>{description}</h5>
       </div>
     </div>
